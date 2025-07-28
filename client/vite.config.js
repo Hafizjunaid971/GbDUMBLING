b688051a35e14bd02bacfff7ea360d6821b403f9
unknown <hafizjunaid971@gmail.com>
2025-07-29 01:18:44 +0500

    deployment done

--- client/vite.config.js
+++ client/vite.config.js
@@ -9,8 +9,9 @@
 
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   base: './',       // relative path for assets
-  plugins: [react()]
+  plugins: [react(), tailwindcss()],
 })

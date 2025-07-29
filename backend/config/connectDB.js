f21c4f3e4c9a975348b5d10049b7db4481197c6b
unknown <hafizjunaid971@gmail.com>
2025-07-30 00:18:36 +0500

    database issue

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,11 +1,26 @@
-import mongoose, { mongo } from "mongoose";
+// import mongoose, { mongo } from "mongoose";
+
+// export const connectDB = async () => {
+//   try {
+//     await mongoose.connect(process.env.MONGOURI);
+//     console.log("MongoDB connected ");
+//   } catch (error) {
+//     console.error("Error connecting to MongoDB:", error);
+//     process.exit(1); // Exit the process with failure
+//   }
+// };
+
+import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGOURI);
-    console.log("MongoDB connected ");
+    const conn = await mongoose.connect(process.env.MONGOURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
-    console.error("Error connecting to MongoDB:", error);
-    process.exit(1); // Exit the process with failure
+    console.error("MongoDB connection error:", error.message);
+    process.exit(1);
   }
 };

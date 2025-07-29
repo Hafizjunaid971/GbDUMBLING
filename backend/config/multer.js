696685ebcf949acc2b5edd2ea90dabdc9eca5e84
unknown <hafizjunaid971@gmail.com>
2025-07-30 02:04:32 +0500

    verce

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -9,27 +9,17 @@
 
 
 // config/multer.js
-import multer from 'multer';
-import { v2 as cloudinary } from 'cloudinary';
-import { CloudinaryStorage } from 'multer-storage-cloudinary';
-import dotenv from 'dotenv';
-
-dotenv.config();
-
-cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_NAME,
-  api_key: process.env.CLOUDINARY_API_KEY,
-  api_secret: process.env.CLOUDINARY_API_SECRET,
-});
+import { v2 as cloudinary } from "cloudinary";
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import multer from "multer";
 
 const storage = new CloudinaryStorage({
   cloudinary,
   params: {
-    folder: 'uploads',
-    allowed_formats: ['jpg', 'jpeg', 'png'],
+    folder: "products",
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
-const upload = multer({ storage });
+export const upload = multer({ storage });
 
-export { upload };

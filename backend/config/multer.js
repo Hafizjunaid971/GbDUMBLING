dcb1522352aeee82b41e18174d73a993f123bf26
unknown <hafizjunaid971@gmail.com>
2025-07-30 00:47:38 +0500

    cloudnary

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -1,8 +1,30 @@
+// import multer from "multer";
+// const storage = multer.diskStorage({
+//   destination: "uploads",
+//   filename: (req, file, cb) => {
+//     return cb(null, `${Date.now()}${file.originalname}`);
+//   },
+// });
+// export const upload = multer({ storage: storage });
+
+
 import multer from "multer";
-const storage = multer.diskStorage({
-  destination: "uploads",
-  filename: (req, file, cb) => {
-    return cb(null, `${Date.now()}${file.originalname}`);
+import { CloudinaryStorage } from "multer-storage-cloudinary";
+import { v2 as cloudinary } from "cloudinary";
+import dotenv from "dotenv";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "your-folder-name", // aapka folder name Cloudinary mein
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
-export const upload = multer({ storage: storage });
+
+export const upload = multer({ storage });
